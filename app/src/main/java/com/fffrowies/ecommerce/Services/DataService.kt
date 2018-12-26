d45485f181de2fbe19335618b0e5518e5ab926a4
fffrowies <f3rnandorowies.saxo@gmail.com>
2018-12-26 16:09:24 -0300

    added info on DataService

--- app/src/main/java/com/fffrowies/ecommerce/Services/DataService.kt
+++ app/src/main/java/com/fffrowies/ecommerce/Services/DataService.kt
@@ -15,6 +15,158 @@ object DataService {
         Product(
             "Jean-Luc Ponty",
             "https://upload.wikimedia.org/wikipedia/commons/3/30/Jean-Luc_Ponty_2008_by_Guillaume_Laurent.jpg",
+            0.00),
+        Product(
+            "Jon Anderson",
+            "",
+            0.00),
+        Product(
+            "Terry Bozzio",
+            "",
+            0.00),
+        Product(
+            "Allan Holdsworth",
+            "",
+            0.00),
+        Product(
+            "Greg Lake",
+            "",
+            0.00),
+        Product(
+            "Adrian Belew",
+            "",
+            0.00),
+        Product(
+            "Phil Collins",
+            "",
+            0.00),
+        Product(
+            "Robert Fripp",
+            "",
+            0.00),
+        Product(
+            "Tony Levin",
+            "",
+            0.00),
+        Product(
+            "Frank Zappa",
+            "",
+            0.00),
+        Product(
+            "Didier Malherbe",
+            "",
+            0.00),
+        Product(
+            "Mino Cinelu",
+            "",
+            0.00),
+        Product(
+            "Napoleon Murphy Brock",
+            "",
+            0.00),
+        Product(
+            "Tommy Mars",
+            "",
+            0.00),
+        Product(
+            "Patrick O'Hearn",
+            "",
+            0.00),
+        Product(
+            "Ruth Underwood",
+            "",
+            0.00),
+        Product(
+            "George Duke",
+            "",
+            0.00),
+        Product(
+            "Chester Thompson",
+            "",
+            0.00),
+        Product(
+            "John Wetton",
+            "",
+            0.00),
+        Product(
+            "Steve Howe",
+            "",
+            0.00),
+        Product(
+            "Rick Wakeman",
+            "",
+            0.00),
+        Product(
+            "Chris Squire",
+            "",
+            0.00),
+        Product(
+            "Peter Gabriel",
+            "",
+            0.00),
+        Product(
+            "Larry Fast",
+            "",
+            0.00),
+        Product(
+            "Jerry Marotta",
+            "",
+            0.00),
+        Product(
+            "Milton McDonald",
+            "",
+            0.00),
+        Product(
+            "Ian McDonald",
+            "",
+            0.00),
+        Product(
+            "Carl Palmer",
+            "",
+            0.00),
+        Product(
+            "Steve Hackett",
+            "",
+            0.00),
+        Product(
+            "Mike Rutherford",
+            "",
+            0.00),
+        Product(
+            "Percy Jones",
+            "",
+            0.00),
+        Product(
+            "Brian Eno",
+            "",
+            0.00),
+        Product(
+            "Miles Davis",
+            "",
+            0.00),
+        Product(
+            "Herbie Hancock",
+            "",
+            0.00),
+        Product(
+            "Wayne Shorter",
+            "",
+            0.00),
+        Product(
+            "Chick Corea",
+            "",
+            0.00),
+        Product(
+            "Miroslav Vitous",
+            "",
+            0.00),
+        Product(
+            "Jack DeJohnette",
+            "",
+            0.00),
+        Product(
+            "Jaco Pastorius",
+            "",
             0.00)
     )
 }
